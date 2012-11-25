7a94ad8e53bff038c8aff8f8436802ef28f56ddf
Adam Cadien <adamcadien@gmail.com>
2012-11-25 04:17:07 -0500

    more problems

--- p104.py
+++ p104.py
@@ -1,10 +1,10 @@
 #!/usr/bin/python
 import sys
 from math import *
+from repr import repr
 
 def pangeafib():
     digs=map(str,range(1,10))
-    print digs
     n1=0
     n2=1
     k=1
@@ -13,23 +13,28 @@ def pangeafib():
         n1=n2
         n2=f
         k+=1
-        if k==150: 
-            print f
-            sfib=str(f)
-            frstdig=sorted(sfib[:9])
-            lastdig=sorted(sfib[-9:])
-            print frstdig
-            print lastdig
+        #sfib=str(f)
+
+        if k%1000==0:
+            print k#,len(sfib)
+
+        ndig=int(log10(f))
+        frstdig=sorted(str(f/10**(ndig-8)))
+        lastdig=sorted(str(f%int(1E9)))
+
+        #Test
+        #if frstdig==digs:
+        #    print "first",k,frstdig,lastdig#,len(sfib)
+        #if lastdig==digs:
+        #    print "last",k,frstdig,lastdig#,len(sfib)
+
+        #Final solution.
+        if frstdig==digs and lastdig==digs:
+            print "win",k,frstdig,lastdig#,len(sfib)
             break
-        sfib=str(f)
-        frstdig=sorted(sfib[:9])
-        lastdig=sorted(sfib[-9:])
-        if frstdig==digs:
-            print "first",k,frstdig,lastdig,len(sfib)
+
+        if k>1000000: 
             break
-        elif lastdig==digs:
-            print "last",k,frstdig,lastdig,len(sfib)
-        if k>1000: break
 
 pangeafib()
 
