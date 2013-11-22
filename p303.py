096eeb3b0d28331921f7c393e2a8bd697bfda004
Adam Cadien <adamcadien@gmail.com>
2013-11-22 15:59:25 -0500

    still too slow

--- p303.py
+++ p303.py
@@ -4,8 +4,20 @@ from numpy import zeros
 
 maxN=10000
 fn=zeros(maxN)
+total=0
 for n in range(1,maxN+1):
+  if n==99:
+    total+=11335578
+    continue
+  if n==999:
+    total+=111333555778
+    continue
+  if n==9999:
+    total+=1111333355557778
+    continue
   mul=1
   while max(str(n*mul))>'2':
     mul+=1
+  total+=mul
   print n,mul
+print total
